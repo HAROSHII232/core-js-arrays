ddb6c70119916dc2a33dfeff381b02bf38ff8a38
Ihar Voitau <igorgrime@gmail.com>
2024-01-08 00:36:57 +0300

    feat: solve getIdentityMatrix task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -397,8 +397,10 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n)
+    .fill(Array(n).fill())
+    .map((xs, i) => xs.map((x, j) => (i === j ? 1 : 0)));
 }
 
 /**
