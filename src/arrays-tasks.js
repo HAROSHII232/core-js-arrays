38b48dc1213d461b7be754c5d89d5434cf54bab3
Ihar Voitau <igorgrime@gmail.com>
2024-01-08 00:35:07 +0300

    feat: solve generateOdds task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -341,8 +341,10 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len * 2 }, (v, i) => i).filter(
+    (el) => el % 2 === 1
+  );
 }
 
 /**
