083da9d9cf6a2b3a6ec36b8c43fb8f531aad939c
Ihar Voitau <igorgrime@gmail.com>
2024-01-15 20:56:58 +0300

    feat: solve sumArrays task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -40,8 +40,14 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    return arr1.map((el, i) => el + arr2[i]);
+  }
+  const biggerArr = arr1.length > arr2.length ? arr1 : arr2;
+  const smallerArr = arr1.length < arr2.length ? arr1 : arr2;
+
+  return biggerArr.map((el, i) => el + (smallerArr[i] ? smallerArr[i] : 0));
 }
 
 /**
